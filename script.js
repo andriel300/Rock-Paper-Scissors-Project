bfa9f5acf0e3be40c2963a611444f1d44b050846
andriel300 <andrielscript@gmail.com>
2022-10-04 19:10:51 -0300

    add input user choices

--- script.js
+++ script.js
@@ -8,3 +8,8 @@ function getComputerChoice() {
     let random = choices[Math.floor(Math.random() * choices.length)];
     return random;
 }
+
+function inputUser(){
+    let UserChoice = prompt("Please use only Paper, rock or scissors to play: ").toLowerCase();
+    return UserChoice;
+}
